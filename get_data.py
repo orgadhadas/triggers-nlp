2fc053709ebe6cce9ea0c4d901bae6e9513baf79
Itay Eilat <itayeilat@lambda.csf.technion.ac.il>
2021-06-03 21:10:40 +0300

    train files

--- get_data.py
+++ get_data.py
@@ -9,14 +9,17 @@ def main():
     single_id_indexer = SingleIdTokenIndexer(lowercase_tokens=True) # word tokenizer
     tokenizer = WordTokenizer(end_tokens=["@@NULL@@"]) # add @@NULL@@ to the end of sentences
     reader = SnliReader(token_indexers={'tokens': single_id_indexer}, tokenizer=tokenizer)
-    dev_dataset = reader.read('https://s3-us-west-2.amazonaws.com/allennlp/datasets/snli/snli_1.0_dev.jsonl')
-    train_dataset = reader.read('https://s3-us-west-2.amazonaws.com/allennlp/datasets/snli/snli_1.0_train.jsonl')
+    datasets_kind = 'dev' # should be dev or train
+    if datasets_kind == 'dev':
+            dataset = reader.read('https://s3-us-west-2.amazonaws.com/allennlp/datasets/snli/snli_1.0_dev.jsonl')
+    else:
+            dataset = reader.read('https://s3-us-west-2.amazonaws.com/allennlp/datasets/snli/snli_1.0_train.jsonl')
     model = load_archive('https://allennlp.s3-us-west-2.amazonaws.com/models/esim-glove-snli-2019.04.23.tar.gz').model
     # model.eval().cuda()
     vocab = model.vocab
 
-    label = 'contradiction'
-    f_without_trigger = open(f"train_data_label_{label}.txt", "w+")
+    label = 'neutral'
+    f_without_trigger = open(f"{datasets_kind}_data_label_{label}.txt", "w+")
     # f_with_trigger = open("triggered_dev_data.txt", "a+")
     # trigger_str = ''
     # for i, idx in enumerate(trigger_token_ids):
@@ -25,8 +28,8 @@ def main():
     #     else:
     #         trigger_str += (' ' + vocab.get_token_from_index(idx))
 
-    for d in train_dataset:
-        sentence = list(d['premise'])
+    for d in dataset:
+        sentence = list(d['hypothesis'])
         if d['label'].label == label:
 
             # print(d['label'].label)
