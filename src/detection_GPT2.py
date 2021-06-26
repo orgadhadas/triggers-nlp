5640d2dd7e99b0e2f15dad4fcae2634843456a79
Itay Eilat <itayeilat@lambda.csf.technion.ac.il>
2021-06-26 19:36:18 +0300

    GPT2 and add parse flags to get_sentence_statistics

--- src/detection_GPT2.py
+++ src/detection_GPT2.py
@@ -17,64 +17,92 @@ def get_args():
     return args
 
 
-def check_if_trigger_exist(ex, tokenizer, model, train_stats, length):
+def check_if_trigger_exist(ex, tokenizer, model, train_stats, num_tokens_in_ex):
     # return 1 if there is a trigger at the beginning.
     loss = get_sentence_loss(model, tokenizer, ex)
-    return loss > train_stats[str(length)]['mean'] + train_stats[str(length)]['std']
+    return loss > train_stats[str(num_tokens_in_ex)]['mean'] + train_stats[str(num_tokens_in_ex)]['std']
+
+
+def find_trigger_location(ex, tokenizer, model, train_stats, num_tokens_in_ex):
+    trigger_length_found = 0
+    try:
+        while check_if_trigger_exist(ex, tokenizer, model, train_stats, num_tokens_in_ex):
+            trigger_length_found += 1
+            ex = ex.split()[1:]
+            ex = " ".join(w for w in ex)
+            num_tokens_in_ex = len(tokenizer(ex)['input_ids'])
+    except:
+        print("missing stats length in find trigger loaction")
+        return -1 # todo: if we consider lack of train statistic as failure of the model we can return trigger_location
+        #todo and hope for good result
+    return trigger_length_found
 
 
 def get_recognition_stats(tokenizer, model, triggers_folder, data_orig, train_stats):
     fp = 0
     tp = 0
-    fn = 0
-    tn = 0
-    correct = 0
+    failure_in_find_trigger_location = 0
+
+
+    total_correct_num_tested_location = 0
     total_num_triggered_sentences = 0
+    # total_num_tested_location_sentences = 0
     tp_rec_per_len_dict = defaultdict(list)
+    acc_per_len_dict = defaultdict(list)
 
     for triggered_file_path, trigger_length in get_tuples_trigger_file_path_num_trigger_list(triggers_folder):
         sentences = get_clean_sentences_from_file(triggered_file_path)
-        total_num_triggered_sentences += len(sentences)
         num_trigger_presence = 0
+        num_correct_trigger_location = 0
+        # num_tested_location_sentences = 0
+        total_num_triggered_sentences += len(sentences)
         for ex in tqdm(sentences):
-            length = len(tokenizer(ex)['input_ids'])
+            num_tokens_in_ex = len(tokenizer(ex)['input_ids'])
             try:
-                if check_if_trigger_exist(ex, tokenizer, model, train_stats, length):
+                if check_if_trigger_exist(ex, tokenizer, model, train_stats, num_tokens_in_ex):
+                    # total_num_triggered_sentences += 1
                     num_trigger_presence += 1
-                    correct += 1
+                trigger_length_found = find_trigger_location(ex, tokenizer, model, train_stats, num_tokens_in_ex)
+                if trigger_length_found != -1:
+                    # num_tested_location_sentences += 1
+                    if trigger_length_found == trigger_length:
+                        num_correct_trigger_location += 1
                 else:
-                    fn += 1
+                    failure_in_find_trigger_location += 1
             except:
-                print(f"No train example with length {length}, skipping...")
+                failure_in_find_trigger_location += 1
+                print(f"No train example with length {num_tokens_in_ex}, skipping...")
                 continue
+        # total_num_tested_location_sentences += num_tested_location_sentences
         tp += num_trigger_presence
-        tp_rec_per_len_dict[trigger_length].append(num_trigger_presence / len(sentences))
+        total_correct_num_tested_location += num_correct_trigger_location
+        acc_per_len_dict[trigger_length].append(num_correct_trigger_location / len(sentences))
+        tp_rec_per_len_dict[trigger_length].append(num_trigger_presence / len(sentences)) #todo: should we devide all sentences??
 
     for trigger_len in tp_rec_per_len_dict.keys():
         mean_tp_rec, std_tp_rec = get_array_mean_std(tp_rec_per_len_dict[trigger_len])
-        print(f"trigger len: {trigger_len}  "
+        mean_acc_location, std_acc_location = get_array_mean_std(acc_per_len_dict[trigger_len])
+        print(f"trigger len: {trigger_len} trigger location mean acc: {mean_acc_location} +- {std_acc_location} "
               f"trigger recognize mean TPR: {mean_tp_rec} +- {std_tp_rec}")
 
 
     print(f"GPT2 TPR: {tp / total_num_triggered_sentences}")
     for ex in tqdm(data_orig):
-        length = len(tokenizer(ex)['input_ids'])
+        num_tokens_in_ex = len(tokenizer(ex)['input_ids'])
         try:
-            if check_if_trigger_exist(ex, tokenizer, model, train_stats, length):
+            if check_if_trigger_exist(ex, tokenizer, model, train_stats, num_tokens_in_ex):
                 fp += 1
-            else:
-                tn += 1
-                correct += 1
         except:
-            print(f"No train example with length {str(length)}, skipping...")
+            print(f"No train example with length {str(num_tokens_in_ex)}, skipping...")
             continue
+    print(f'trigger location acc: {total_correct_num_tested_location / total_num_triggered_sentences}')
+    print(f"number of failure in find trigger location due missing statistics {failure_in_find_trigger_location}")
+    print(f"% of failure in find trigger location due missing statistics {failure_in_find_trigger_location / total_num_triggered_sentences}")
+    print(f'trigger location acc if we consider only example that did not fail due missing statistics'
+          f': {total_correct_num_tested_location / (total_num_triggered_sentences- failure_in_find_trigger_location)}')
 
     print(f"GPT2 FPR: {fp / len(data_orig)}")
-    num_total_sentences = total_num_triggered_sentences + len(data_orig)
-    recall = tp / (tp + fn)
-    precision = tp / (tp + fp)
-    acc = correct / num_total_sentences
-    f1 = (precision * recall) / (precision + recall)
+
 
     # print(f"Recall: {recall}, Precision: {precision}, F1: {f1}, Accuracy: {acc}")
 
--- src/get_sentence_statistics.py
+++ src/get_sentence_statistics.py
@@ -2,41 +2,45 @@ import json
 from collections import defaultdict
 
 import numpy as np
+import random
 from tqdm import tqdm
 from transformers import AutoTokenizer, AutoModelWithLMHead
+import argparse
 
-from Utils import clean_spaces, get_sentence_loss, parse_args
-
-with open("../data/train_data_label_entailment_uniq.txt") as f:
-    entailment = f.readlines()
-
-# with open("../data/train_data_label_neutral_uniq.txt") as f:
-#     neutral = f.readlines()
-
-# with open("../data/train_data_label_contradiction_uniq.txt") as f:
-#     contradiction = f.readlines()
+from Utils import get_clean_sentences_from_file, get_sentence_loss, parse_args
 
 
-sentences = entailment
-
-sentences_new = list(map(lambda x : x[:-10], sentences))
-sentences_new = list(map(lambda x : clean_spaces(x), sentences_new))
-tokenizer = AutoTokenizer.from_pretrained("gpt2")
-model = AutoModelWithLMHead.from_pretrained("gpt2")
-
-d = defaultdict(list)
-
-for s in tqdm(sentences_new):
-    # log_proba = get_sentence_log_proba(model, tokenizer, s)
-    loss = get_sentence_loss(model, tokenizer, s)
-    # d[len(tokenizer(s)['input_ids'])].append(log_proba)
-    d[len(tokenizer(s)['input_ids'])].append(loss)
+def get_parsed_input_path():
+    parser = argparse.ArgumentParser(description='Get sentence statistics (mean log proba, std) for each length.')
+    parser.add_argument('--input_path', '-i', required=True, type=str, help='input path')
+    parser.add_argument('--output_path', '-o', required=True, type=str, help='save path')
+    args = parser.parse_args()
+    return args
 
 
-output_dict = {}
-for length in d.keys():
-    output_dict[length] = {"mean": np.mean(d[length]), "std": np.std(d[length]), "N": len(d[length])}
+def main(args):
+	sentences = get_clean_sentences_from_file(args.input_path)
+	random.shuffle(sentences)
+	tokenizer = AutoTokenizer.from_pretrained("gpt2")
+	model = AutoModelWithLMHead.from_pretrained("gpt2")
 
-args = parse_args()
-with open(args.path, 'w') as json_file:
-    json.dump(output_dict, json_file, sort_keys=True)
+	d = defaultdict(list)
+
+	for s in tqdm(sentences):
+	    # log_proba = get_sentence_log_proba(model, tokenizer, s)
+	    loss = get_sentence_loss(model, tokenizer, s)
+	    # d[len(tokenizer(s)['input_ids'])].append(log_proba)
+	    d[len(tokenizer(s)['input_ids'])].append(loss)
+
+
+	output_dict = {}
+	for length in d.keys():
+	    output_dict[length] = {"mean": np.mean(d[length]), "std": np.std(d[length]), "N": len(d[length])}
+
+	with open(args.output_path, 'w') as json_file:
+	    json.dump(output_dict, json_file, sort_keys=True)
+
+
+if __name__ == '__main__':
+    args = get_parsed_input_path()
+    main(args)
