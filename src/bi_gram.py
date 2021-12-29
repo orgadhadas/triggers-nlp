09d02f010bc9f5291233b24223111a65d658c80d
Hadas Orgad <orgad.hadas@newton.csf.technion.ac.il>
2021-12-29 10:21:29 +0200

    leftovers from previos commit

--- src/bi_gram.py
+++ src/bi_gram.py
@@ -1,3 +1,4 @@
+import pickle
 from collections import defaultdict, Counter
 import argparse
 import json
@@ -5,9 +6,30 @@ import torchtext as tt
 import wandb
 from tqdm import tqdm
 import numpy as np
+import pandas as pd
+from Utils import get_sentences_from_file
 
-from Utils import get_clean_sentences_from_file, get_tuples_trigger_file_path_num_trigger_list, parse_args
+def parse_args():
+    parser = argparse.ArgumentParser(description='Get model result.')
+    parser.add_argument('--triggered', '-t', required=True, type=str, help='The file with triggeres')
+    parser.add_argument('--clean', '-c', required=True, type=str, help='The file with orignal sentences (no trigger)')
+    parser.add_argument('--thr', required=True, type=float, help='Threshold for bi-gram')
+    parser.add_argument('--train', required=False, default=None, type=str, help='If you want to train a new bi-gram model, input the file to train from')
+    parser.add_argument('--bigram', required=False, default=None, type=str, help='Path to bi-gram json file')
+    args = parser.parse_args()
+    return args
 
+def init_wandb(args):
+    wandb.init(project="triggers-detection-test", config={
+        "model": "n-gram",
+        "threshold": args.thr,
+        "triggered_file": args.triggered,
+        "clean_file": args.clean,
+        "n": 2,
+        "train_data": args.train,
+        "model_path": args.bigram,
+        "with_eos": True
+    })
 
 def train_bi_gram_model(input_path, output_path):
     '''
@@ -15,12 +37,20 @@ def train_bi_gram_model(input_path, output_path):
     :param output_path: Output path to dump the dictionary
     :return:
     '''
+
+    print("Training a bi-gram model using file " + input_path)
+
     bi_gram_dict = defaultdict(lambda: defaultdict(int))
     counter_first_word = Counter()
-    sentences = get_clean_sentences_from_file(input_path)
+    sentences = get_sentences_from_file(input_path)
+    tokenizer = tt.data.get_tokenizer("basic_english")
 
     for s in tqdm(sentences):
-        tokens_list = tt.data.get_tokenizer("basic_english")(s)
+        tokens_list = tokenizer(s)
+
+        # TMP - but maybe make this permanent
+        tokens_list.append("#eos#")
+        tokens_list.insert(0, "#eos#")
 
         for (w1, w2) in zip(tokens_list[:-1], tokens_list[1:]):
             counter_first_word[w1] += 1
@@ -30,12 +60,14 @@ def train_bi_gram_model(input_path, output_path):
         for w2 in list(bi_gram_dict[w1].keys()):
             bi_gram_dict[w1][w2] = bi_gram_dict[w1][w2] / counter_first_word[w1]
 
-    with open(output_path, 'w') as json_file:
-        json.dump(bi_gram_dict, json_file, indent=4)
+    import pickle
+    with open(output_path, 'wb') as handle:
+        pickle.dump(dict(bi_gram_dict), handle, protocol=pickle.HIGHEST_PROTOCOL)
+
     return bi_gram_dict
 
 
-def test(path, b_dict, gt_trigger_ends_location=0):
+def test(path, b_dict, thr):
     """
     :param path: Path to take sentences from
     :param b_dict: The bigram dictionary.
@@ -46,108 +78,108 @@ def test(path, b_dict, gt_trigger_ends_location=0):
     num_sentences: The number of sentences in the file.
     """
 
-    num_attacked = 0
+    num_attack_detected = 0
     tokenizer = tt.data.get_tokenizer("basic_english")
-    sentences = get_clean_sentences_from_file(path)
-    if gt_trigger_ends_location != -1:
-        triggered_word = sentences[0].split()[:gt_trigger_ends_location]
-        triggered_word = " ".join(triggered_word)
-        gt_trigger_ends_location = len(tokenizer(triggered_word))
+    data = pd.read_csv(path, header=None)
 
     num_recognized_trigger_location = 0
-    for s in sentences:
-        tokens_list = tokenizer(s)
+    for _, (sentence, trigger) in data.iterrows():
+
+        gt_trigger_ends_location = len(tokenizer(trigger)) + 1 # Index of the last word in the trigger + 1
+        tokens_list = tokenizer(sentence)
+
+        # TMP - but maybe make this permanent
+        tokens_list.append("#eos#")
+        tokens_list.insert(0, "#eos#")
+
         attacked = 0
         trigger_location = 0
         for i, (w1, w2) in enumerate(zip(tokens_list[:-1], tokens_list[1:])):
-            if b_dict[w1][w2] == 0:
+            if b_dict[w1][w2] <= thr:
                 trigger_location += 1
                 attacked = 1
             else:
                 break
 
-        num_attacked += attacked
+        num_attack_detected += attacked
+        # print(trigger_location, gt_trigger_ends_location)
+        if attacked == 0:
+            for i, (w1, w2) in enumerate(zip(tokens_list[:-1], tokens_list[1:])):
+                print(w1, w2, b_dict[w1][w2])
         if trigger_location == gt_trigger_ends_location:
             num_recognized_trigger_location += 1
 
-    return num_attacked, num_recognized_trigger_location, len(sentences)
+    return num_attack_detected, num_recognized_trigger_location, len(data)
+
+def test_not_triggered(path, b_dict, thr):
+    """
+    :param path: Path to take sentences from
+    :param b_dict: The bigram dictionary.
+    :param gt_trigger_ends_location: Where triggers tokens ends!
+    :return:
+    num_attacked: The number of the sentences that model recognized that consist trigger.
+    num_recognized_trigger_location: The number of the trigger locations that were find correctly.
+    num_sentences: The number of sentences in the file.
+    """
+
+    num_attack_detected = 0
+    tokenizer = tt.data.get_tokenizer("basic_english")
+
+    sentences = get_sentences_from_file(path)
+
+    for sentence in sentences:
+
+        tokens_list = tokenizer(sentence)
+
+        # TMP - but maybe make this permanent
+        tokens_list.append("#eos#")
+        tokens_list.insert(0, "#eos#")
+
+        attacked = 0
+        trigger_location = 0
+        for i, (w1, w2) in enumerate(zip(tokens_list[:-1], tokens_list[1:])):
+            # print(w1, w2, b_dict[w1][w2])
+            if b_dict[w1][w2] <= thr:
+                trigger_location += 1
+                attacked = 1
+            else:
+                break
+
+        num_attack_detected += attacked
+
+    return num_attack_detected, 0, len(sentences)
 
 
 def main(args):
 
-    thr = 0
-    wandb.init(project="triggers-detection-test", config={
-        "model": "n-gram",
-        "threshold": thr,
-        "n": 2
-    })
+    init_wandb(args)
 
-    b_dict = train_bi_gram_model(r'../data/train_data_label_entailment_uniq.txt', r'../data/bi_gram_entailment_dict.json')
-    entailment_clean_path = args.clean
-    acc_per_len_dict = defaultdict(list)
-    tp_rec_per_len_dict = defaultdict(list)
+    if args.train is not None:
+        b_dict = train_bi_gram_model(args.train, args.bigram)
+    else:
+        with open(args.bigram, 'rb') as handle:
+            b_dict = pickle.load(handle)
+            b_dict = defaultdict(lambda: defaultdict(int), b_dict)
+        print(f"Loaded bi-gram model from {args.bigram}")
 
-    tp_trigger_presence, total_num_correct_trigger_location, total_num_sentences = 0, 0, 0
-    triggers_dir_path = args.dir
-    all_triggers = []
-    for triggered_file_path, trigger_length, trigger_name in get_tuples_trigger_file_path_num_trigger_list(triggers_dir_path):
-        all_triggers.append(trigger_name)
-        num_trigger_presence, num_correct_trigger_location, num_sentences = test(triggered_file_path, b_dict, gt_trigger_ends_location=trigger_length)
-        tp_trigger_presence += num_trigger_presence
-        total_num_correct_trigger_location += num_correct_trigger_location
-        total_num_sentences += num_sentences
-        acc_per_len_dict[trigger_length].append(num_correct_trigger_location / num_sentences)
-        tp_rec_per_len_dict[trigger_length].append(num_trigger_presence / num_sentences)
-    print("bigram")
-    print(f"acc over all triggered sentences: {total_num_correct_trigger_location/total_num_sentences}")
-    mean_location_acc_dict = dict()
-    std_location_acc_dict = dict()
-    mean_tp_rec_dict = dict()
-    std_tp_rec_dict = dict()
+    tp_trigger_presence, num_correct_trigger_location, num_sentences = test(args.triggered, b_dict, args.thr)
 
-    acc_mean_per_len = [0] * len(acc_per_len_dict)
-    acc_std_per_len = [0] * len(acc_per_len_dict)
-    tpr_mean_per_len = [0] * len(acc_per_len_dict)
-    tpr_std_per_len = [0] * len(acc_per_len_dict)
-
-    for trigger_len in acc_per_len_dict.keys():
-        mean_acc_location = np.array(acc_per_len_dict[trigger_len]).mean()
-        mean_location_acc_dict[trigger_len] = mean_acc_location
-        std_acc_location = np.array(acc_per_len_dict[trigger_len]).std()
-        std_location_acc_dict[trigger_len] = std_acc_location
-        mean_tp_rec = np.array(tp_rec_per_len_dict[trigger_len]).mean()
-        mean_tp_rec_dict[trigger_len] = mean_tp_rec
-        std_tp_rec = np.array(tp_rec_per_len_dict[trigger_len]).std()
-        std_tp_rec_dict[trigger_len] = np.array(std_tp_rec)
-        print(f"trigger len: {trigger_len} trigger location mean acc: {mean_acc_location} +- {std_acc_location} "
-              f"trigger recognize mean TPR: {mean_tp_rec} +- {std_tp_rec}")
-        acc_mean_per_len[trigger_len-1] = mean_acc_location
-        acc_std_per_len[trigger_len-1] = std_acc_location
-        tpr_mean_per_len[trigger_len-1] = mean_tp_rec
-        tpr_std_per_len[trigger_len-1] = std_tp_rec
-        wandb.log({"Mean_acc_per_length": mean_acc_location, "Mean_tpr_per_length": mean_tp_rec, "len": trigger_len})
-
-
-    tpr_trigger_presence = tp_trigger_presence / total_num_sentences
-    print(F"bigram TPR: {tpr_trigger_presence}")
-    print(f'trigger location acc: {total_num_correct_trigger_location/total_num_sentences}')
+    acc = num_correct_trigger_location / num_sentences
+    tpr_trigger_presence = tp_trigger_presence / num_sentences
 
     # Note that positive is triggered sentence. So false positive is the number of sentences that model think they are
     # triggered. We know that fp_trigger_presence is false because the input is clean entailment (no trigger at the
     # beginning).
-    fp_trigger_presence, _, num_false_sentences = test(entailment_clean_path, b_dict)
-    FPR = fp_trigger_presence/num_false_sentences
+    fp_trigger_presence, _, num_false_sentences = test_not_triggered(args.clean, b_dict, args.thr)
+    FPR = fp_trigger_presence / num_false_sentences
+
     print(f"bigram FPR: {FPR}")
+    print(f"bigram TPR: {tpr_trigger_presence}")
+    print(f"bigram ACC: {acc}")
+
     wandb.summary["FPR"] = FPR
-    wandb.summary["ACC_per_length_mean"] = acc_mean_per_len
-    wandb.summary["ACC_per_length_std"] = acc_std_per_len
-    wandb.summary["ACC_mean_overall"] = np.mean(acc_mean_per_len)
-    wandb.summary["ACC_std_overall"] = np.std(acc_mean_per_len)
-    wandb.summary["TPR_per_length_mean"] = tpr_mean_per_len
-    wandb.summary["TPR_per_length_std"] = tpr_std_per_len
-    wandb.summary["TPR_mean_overall"] = np.mean(tpr_mean_per_len)
-    wandb.summary["TPR_std_overall"] = np.std(tpr_mean_per_len)
-    wandb.summary["inspected_triggers"] = all_triggers
+    wandb.summary["ACC"] = acc
+    wandb.summary["TPR"] = tpr_trigger_presence
 
 if __name__ == '__main__':
     args = parse_args()
